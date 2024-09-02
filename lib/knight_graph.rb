81c6b2c0c326ed60bb2900adedc0731ccf9b712a
aheadtuna <11willi.gamer@gmail.com>
2024-09-02 20:48:05 +0200

    Add Plan and Functions in knight_graph class buildgraph which has a position as input value and succesfull creates a directed graph with all posible moves from the position, and  also test Function which proved the succes

--- lib/knight_graph.rb
+++ lib/knight_graph.rb
@@ -1,32 +1,44 @@
 require_relative 'node'
 
 class KnightGraph
-  attr_reader :root
+  attr_accessor :root, :visited_positions
 
-  def initialize(root_position)
-    @root = build_graph(root_position)
+  def initialize(position)
+    @visited_positions = {}
+    @root = build_graph(position)
   end
 
-  def build_graph(position) # rubocop:disable Metrics/AbcSize
+  def build_graph(position) # rubocop:disable Metrics/AbcSize,Metrics/MethodLength
     return nil unless on_board?(position)
+    return nil if @visited_positions[position] # Avoid revisiting nodes
 
     node = Node.new(position)
+    @visited_positions[position] = node # Mark position as visited
+
     y = position[0]
     x = position[1]
 
-    node.nextnodes = [build_graph([y - 1, x - 2]), build_graph([y + 1, x - 2]),
-                      build_graph([y - 1, x + 2]), build_graph([y + 1, x + 2]),
-                      build_graph([y + 2, x - 1]), build_graph([y + 2, x + 1]),
-                      build_graph([y - 2, x + 1]), build_graph([y - 2, x - 1])]
+    possible_moves = [
+      [y - 1, x - 2], [y + 1, x - 2],
+      [y - 1, x + 2], [y + 1, x + 2],
+      [y + 2, x - 1], [y + 2, x + 1],
+      [y - 2, x + 1], [y - 2, x - 1]
+    ]
+
+    node.nextnodes = possible_moves.map { |new_position| build_graph(new_position) }.compact
 
     node
   end
 
-  def on_board?(position)
-    position.each do |axes|
-      return false if axes > 7 || axes.negative?
+  def check(graph)
+    until graph.nil?
+      p graph.nextnodes << "s"
+      graph = graph.nextnodes[1]
     end
-    true
+  end
+
+  def on_board?(position)
+    position.all? { |axis| axis.between?(0, 7) }
   end
 end
 
